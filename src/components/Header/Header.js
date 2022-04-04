9c257ea54e9781e089c9316d840325d7ead7a020
mdsafiulazam <mdriadkhan20@gmail.com>
2022-04-04 22:37:00 +0600

    menu bar is done with style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <h2>this is a header section</h2>
+        <div className='container'>
+            <div className='logo'>
+            <h2><span className='color'>আমার </span>বই ঘর</h2>
+            </div>
+            <nav className='menu-bar'>
             <Link to='/home'>Home</Link>
             <Link to='/reviews'>Reviews</Link>
             <Link to='/dashboard'>DashBoard</Link>
+            </nav>
         </div>
     );
 };
